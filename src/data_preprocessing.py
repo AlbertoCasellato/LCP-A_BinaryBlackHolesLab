fcb414717f60752368f3ebf93f9f09714b188405
Alberto Casellato <alberto.casellato@studenti.unipd.it>
2025-01-31 17:25:02 +0100

    Modified graph for stackplot for all alpha

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -15,3 +15,13 @@ def load_data(folder):
         dict_dataset[filename1] = list_dataset
     
     return dict_dataset
+
+def get_parameter(dataset, column_name, alpha=None):
+    list_metallicities = ["0.0002", "0.0004", "0.0008", "0.0012", "0.0016", "0.002", "0.004", "0.006", "0.008", "0.012", "0.016", "0.02"]
+    if alpha is not None:
+        return [dataset[str(alpha)][metallicity][column_name] for metallicity in list_metallicities]
+    else:
+        # TODO: think about keeping this or not
+        list_alpha = ["A0.5", "A1", "A3", "A5"]
+        for alpha in list_alpha:
+            pass
